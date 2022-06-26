ad297f5752fd5fbeeaedd1f6a0b94137c7f91754
Evgenii Poperezhai <zenap@orthazone.com>
2022-06-26 22:34:57 +0300

    - add events

--- Application.php
+++ Application.php
@@ -11,6 +11,11 @@ use eapdob\phpmvc\db\Database;
  */
 class Application
 {
+    const EVENT_BEFORE_REQUEST = 'beforeRequest';
+    const EVENT_AFTER_REQUEST = 'afterRequest';
+
+    protected array $eventListeners = [];
+
     public static string $ROOT_DIR;
     public static Application $app;
 
@@ -53,6 +58,7 @@ class Application
 
     public function run()
     {
+        $this->triggerEvent(self::EVENT_BEFORE_REQUEST);
         try {
             echo $this->router->resolve();
         } catch (\Exception $e) {
@@ -75,5 +81,18 @@ class Application
     {
         $this->user = null;
         $this->session->remove('user');
+    }
+
+    public function on($eventName, $callback)
+    {
+        $this->eventListeners[$eventName] = $callback;
+    }
+
+    public function triggerEvent($eventName)
+    {
+        $callbacks = $this->eventListeners[$eventName] ?? [];
+        foreach ($callbacks as $callback) {
+            call_user_func($callback);
+        }
     }
 }
